252ea4e75e95fb8293bb18b2693adac29e49bb94
Ryan Egan <eganrya@gmail.com>
2012-05-24 15:41:36 -0400

    Added Swipe Events

--- jquery.jResponsoSlider.js
+++ jquery.jResponsoSlider.js
@@ -3,148 +3,150 @@
  * Author:		Ryan Egan
  */
 
-;(function ( $, window, document, undefined ) {
-	
+;(function ($, window, document, undefined) {
     // Create the defaults once
     var pluginName = 'jResponsoSlider',
-        defaults = {
-            fadeSpeed: 1000,
-            timeBetweenSlides: 4000
-        };
+    	defaults = {
+			fadeSpeed: 1000,
+			timeBetweenSlides: 7000
+		};
 
     // Constructor
-    function Plugin( element, options ) {
+    function Plugin(element, options) {
         this.element = element;
-        this.options = $.extend( {}, defaults, options) ;
-        this._defaults = defaults;
-        this._name = pluginName;
-        this._index = 0;
-        this._$slides = $(element).children('li');
-        this._length = this._$slides.size();
+        this.options = $.extend({}, defaults, options);
+        this.defaults = defaults;
+        this.name = pluginName;
+
+        //Helper Vars
+        this.index = 0;
+        this.slides = $(element).children('li');
+        this.length = this.slides.size();
+        
+        //Init Plugin
         this.init();
     }
 
     Plugin.prototype = {
-        
-        /*
-         * Function: init
-         */
-        init: function() {
-            
-            //CSS Arrays
-        	show = {"float": "left", "position": "relative"};
-        	hide = {"float": "none", "position": "absolute"};
-        	
-        	this._$slides.css(hide); //hide all
-        	this._$slides.eq(this._index).css(show); //show first
-        	
-          	this.initSlideShow();
-          	this.initEventHandlers();
+		/*
+		* Function: init
+		*/
+        init: function () {
+			
+			var $this = this;
+			show = {"float": "left", "position": "relative"};
+			hide = {"float": "none", "position": "absolute"};
+
+			//Start the slideshow
+			this.initSlideShow();
+			
+			//Event Handlers
+			$(this.element)
+				.on('swipeLeft', function () {
+					$this.prevSlide();
+  				})
+  				.on('swipeRight', function () {
+  					$this.nextSlide();
+  				});
+
+        	this.slides.css(hide); //hide all
+        	this.slides.eq(this.index).css(show); //show first
         }, 
-        
-        
-      	/*
-      	 * Function: initSlideShow
-      	 */
+
+		/*
+		* Function: initSlideShow
+		* ----------------------------
+		* Initiates the slide show
+		*/
         initSlideShow: function () {
-        	$this = this;
-    		rotate = setInterval(function () {
-    			
-    			//Increment index or reset back at 0 if end
-    			if($this._index + 1 < $this._length){
-    				id = $this._index + 1
-    			} else {
-    				id = 0;
+			$this = this;
+
+    		slideShow = setInterval( function () {
+				var id;
+				//Increment index or reset back at 0 if end
+				if ($this.index + 1 < $this.length) {
+					id = $this.index + 1;
+				} else {
+					id = 0;
     			}
-    			
-  				$this.slideTo($this, id);
-  				
-            }, this._defaults.timeBetweenSlides);
-      	},
-      	
-      	/*
-      	 * Function: initEventHandlers
-      	 */
-        initEventHandlers: function () {
 
-			var X,
-          		Y,
-          		$this = $(this.element);
+				$this.slideTo(id);
 
-        	//Touch Events
-			$this.bind('touchstart', touchstart);
-			
-			
-			//Touch Start
-			function touchstart(event) {
-	    		var touches = event.originalEvent.touches;
-	    		if (touches && touches.length) {
-	      			X = touches[0].pageX;
-	      			Y = touches[0].pageY;
-	      			$this.bind('touchmove', touchmove);
-	    		}
-	    		event.preventDefault();
-  			}
-  			
-  			//Detect Swipe
-  			function touchmove(event) {
-        		var touches = event.originalEvent.touches;
-        		if (touches && touches.length) {
-          			var deltaX = X - touches[0].pageX;
-          
-	          		if (dX >= 50) {
-	            		$this.trigger("swipeLeft");
-	          		}
-	          		if (dX <= -50) {
-	            		$this.trigger("swipeRight");
-	          		}
-	          		
-	          		if (Math.abs(dX) >= 50 || Math.abs(dY) >= 50) {
-	            		$this.unbind('touchmove', touchmove);
-	          		}
-        		}
-        		event.preventDefault();
-      		}
-      		
-      		
-      		//Testing the swipe
-      		$this.on('swipeLeft', function(){
-      			alert('swiped-left');
-      		});
-      		
-      		$this.on('swipeRight', function(){
-      			alert('swiped-right');
-      		});
-      		
-      	}, //END initEventHandlers
-      	
-      	
-      	/*
-      	 * Function: slideTo
-      	 */
-      	slideTo: function (_this, id) {
-      		
-      		_this._$slides
-  				.stop()
-				.slideUp(this._defaults.fadeSpeed, function () {
+			}, this.options.timeBetweenSlides);
+		},
+
+		/*
+		* Function: slideTo
+		* ----------------------------
+		* Slides to a certain slide
+		*/
+      	slideTo: function (id) {
+
+			this.slides
+				.stop() //hide all
+				.fadeOut(this.options.fadeSpeed, function () {
 					$(this).css(hide);
 				})
-				.eq(id)
-					.css(show)
-					.slideDown(this._defaults.fadeSpeed, function () {
+				.eq(id) // show the current one
+					.fadeIn(this.options.fadeSpeed, function () {
 						$(this).css(show);
 					});
-      		_this._index = id;
-      	}
-    };
+			this.index = id; //set the id
 
-    $.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName,
-                new Plugin( this, options ));
-            }
-        });
-    }
+		}, //END slideto()
 
+		/*
+		* Function: nextSlide
+		* ----------------------------
+		* Goes to next slide (forwards)
+		*/
+      	nextSlide: function () {
+
+			var id;
+
+			//Increment index or reset back at 0 if end
+			if ( this.index + 1 < this.length ) {
+				id = this.index + 1;
+			} else {
+				id = 0;
+			}
+			
+			clearInterval(slideShow); //clear the auto slideshow
+			this.slideTo(id);
+			this.initSlideShow(); //reinit slideshow
+
+      	}, //END nextSlide()
+      	
+		/*
+      	* Function: prevSlide
+		* ----------------------------
+		* Goes to previous slide (backwards)
+		*/
+		prevSlide: function () {
+			var id;
+			//Decrement index or reset back at len if start
+			if (this.index - 1 >= 0) {
+				id = this.index - 1;
+			} else {
+				id = this.length - 1;
+			}
+	    			
+			clearInterval(slideShow); //clear the auto slideshow
+			this.slideTo(id);
+			this.initSlideShow(); //reinit slideshow
+		} //END prevSlide()
+	};
+
+	/*
+	* Plugin Wrapper
+	* ----------------------------
+	* This was written by someone much smarter than myself but it I know it does important stuff
+	*/
+	$.fn[pluginName] = function (options) {
+		return this.each(function () {
+			if (!$.data(this, 'plugin_' + pluginName)) {
+				$.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+			}
+		});
+	}
 })( jQuery, window, document );
